65cbfdab5b2db7e2fd6e8cfda57052cd531c771b
czp3009 <czp3009@gmail.com>
2021-08-18 15:07:38 +0800

    static resource request handler

--- ServerMonitor/Web/BackEnd/StaticResourceRequestHandler.cs
+++ ServerMonitor/Web/BackEnd/StaticResourceRequestHandler.cs
@@ -1,12 +1,73 @@
-﻿using System.Net;
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
 
 namespace ServerMonitor.Web.BackEnd
 {
     public class StaticResourceRequestHandler : IRequestHandler
     {
+        private static readonly Dictionary<string, string> MimeTable = new Dictionary<string, string>()
+        {
+            { "html", "text/html" },
+            { "js", "application/x-javascript" },
+            { "map", "application/json" }
+        };
+
+        private readonly Assembly _assembly = Assembly.GetExecutingAssembly();
+
         public bool TryHandle(HttpListenerContext context)
         {
-            return false;
+            var request = context.Request;
+            if (request.HttpMethod != "GET") return false;
+            var uri = request.Url;
+            var path = uri.AbsolutePath;
+            Stream stream = null;
+            try
+            {
+                var name = $"ServerMonitor.Web.FrontEnd{path.Replace('/', '.')}";
+                stream = _assembly.GetManifestResourceStream(name);
+            }
+            catch (Exception)
+            {
+                // ignored
+            }
+
+            if (stream == null) return false;
+
+            async void Handle()
+            {
+                var response = context.Response;
+                var contentType = TryGetContentType(uri.Segments.Last());
+                if (contentType != null) response.ContentType = contentType;
+                response.ContentLength64 = stream.Length;
+                var outputStream = response.OutputStream;
+                try
+                {
+                    await stream.CopyToAsync(outputStream);
+                }
+                finally
+                {
+                    stream.Close();
+                    outputStream.Close();
+                    response.Close();
+                }
+            }
+
+            Handle();
+
+            return true;
+        }
+
+        private static string TryGetContentType(string fileName)
+        {
+            var indexOfLastDot = fileName.LastIndexOf('.');
+            if (indexOfLastDot < 0 || indexOfLastDot == fileName.Length - 1) return null;
+            var extension = fileName.Substring(indexOfLastDot + 1);
+            MimeTable.TryGetValue(extension, out var contentType);
+            return contentType;
         }
     }
 }
