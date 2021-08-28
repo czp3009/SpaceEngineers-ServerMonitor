b3a06ede9685a67b769424da40413ba4d9719e5d
czp3009 <czp3009@gmail.com>
2021-08-28 13:33:42 +0800

    optimize about page css

--- ServerMonitor/Web/FrontEnd/src/pages/AboutPage.jsx
+++ ServerMonitor/Web/FrontEnd/src/pages/AboutPage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import {Box, Container, Grid, Link, makeStyles, Theme, Typography} from "@material-ui/core";
+import {Box, Container, Link, makeStyles, Theme, Typography} from "@material-ui/core";
 
 const useStyles = makeStyles((theme: Theme) => ({
     root: {
@@ -18,21 +18,19 @@ export default function () {
     return (
         <Box className={classes.root}>
             <Container>
-                <Grid container direction="column">
-                    <Typography variant="h4">Space Engineers Server Monitor</Typography>
-                    <Typography>
-                        This is a server plugin aimed to provide interactive web-based monitor for players.
-                    </Typography>
-                    <Typography>
-                        If you find bugs or have any idea, please visit our
-                        {" "}
-                        <Link href="https://github.com/czp3009/SpaceEngineers-ServerMonitor" target="_blank">
-                            code repository
-                        </Link>
-                        {" "}
-                        and feel free to create an issue.
-                    </Typography>
-                </Grid>
+                <Typography variant="h4">Space Engineers Server Monitor</Typography>
+                <Typography>
+                    This is a server plugin aimed to provide interactive web-based monitor for players.
+                </Typography>
+                <Typography>
+                    If you find bugs or have any idea, please visit our
+                    {" "}
+                    <Link href="https://github.com/czp3009/SpaceEngineers-ServerMonitor" target="_blank">
+                        code repository
+                    </Link>
+                    {" "}
+                    and feel free to create an issue.
+                </Typography>
             </Container>
         </Box>
     )
