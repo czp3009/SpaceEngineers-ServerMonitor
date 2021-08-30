b057f09af6a299fc626dd79e08ccc6475a78948a
czp3009 <czp3009@gmail.com>
2021-08-30 09:19:15 +0800

    use display none

--- ServerMonitor/Web/FrontEnd/src/components/ActionBar.jsx
+++ ServerMonitor/Web/FrontEnd/src/components/ActionBar.jsx
@@ -1,8 +1,13 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
 import {Box, IconButton, makeStyles, Menu, MenuItem, Theme, Typography} from "@material-ui/core";
 import {useInView} from "react-intersection-observer";
 import MoreVertIcon from "@material-ui/icons/MoreVert";
 import ToolbarWithBottomBorder from "./ToolbarWithBottomBorder";
+
+const display = {
+    true: "flex",
+    false: "none"
+}
 
 const useStyles = makeStyles((theme: Theme) => ({
     buttonArea: {
@@ -23,7 +28,9 @@ export default function (
     }: { icon: React.ReactNode, title: string, subTitle: string, children?: React.ReactNode }
 ) {
     const classes = useStyles()
+    const buttonAreaContainerRef = useRef()
     const {ref: buttonAreaRef, inView: buttonAreaInView} = useInView({
+        root: buttonAreaContainerRef.current,
         initialInView: true,
         threshold: 1
     })
@@ -52,7 +59,7 @@ export default function (
 
     return (
         <ToolbarWithBottomBorder variant="dense">
-            <Box display="flex" alignItems="center" borderRight="1px solid rgba(0, 0, 0, 0.12)">
+            <Box display={display[buttonAreaInView]} alignItems="center" borderRight="1px solid rgba(0, 0, 0, 0.12)">
                 <Box mr={4} display="flex" alignItems="center">
                     {icon}
                     <Box ml={2}>
@@ -61,13 +68,13 @@ export default function (
                 </Box>
             </Box>
             <Box display="flex" alignItems="center">
-                <Box ml={2} mr={6}>
+                <Box ml={buttonAreaInView ? 2 : 0} mr={buttonAreaInView ? 6 : 0}>
                     <Typography variant="h6" noWrap>{subTitle}</Typography>
                 </Box>
             </Box>
-            <Box display="flex" flex="auto" alignItems="center">
-                <Box ref={buttonAreaRef} className={classes.buttonArea} display="flex" flex="auto" alignItems="center"
-                     visibility={buttonAreaInView ? "visible" : "hidden"} overflow="hidden">
+            <Box ref={buttonAreaContainerRef} display="flex" flex="auto" alignItems="center">
+                <Box ref={buttonAreaRef} className={classes.buttonArea} display={display[buttonAreaInView]} flex="auto"
+                     alignItems="center" overflow="hidden">
                     <Box display="flex" flex="auto" alignItems="center">
                         {leftButtonAreaElements}
                     </Box>
@@ -76,19 +83,13 @@ export default function (
                     </Box>
                 </Box>
             </Box>
-            {
-                !buttonAreaInView &&
-                <Box position="absolute" width="100%" display="flex" flex="auto" alignItems="center"
-                     paddingRight={{"xs": 3, "sm": 4}}>
-                    <Box display="flex" flex="auto" alignItems="center" justifyContent="flex-end">
-                        <IconButton onClick={event => setAnchorEl(event.currentTarget)}>
-                            <MoreVertIcon/>
-                        </IconButton>
-                    </Box>
-                </Box>
-            }
-            <Menu anchorEl={anchorEl} keepMounted open={anchorEl != null}
-                  onClose={() => setAnchorEl(null)} onClick={() => setAnchorEl(null)}>
+            <Box display={display[!buttonAreaInView]} flex="auto" alignItems="center" justifyContent="flex-end">
+                <IconButton onClick={event => setAnchorEl(event.currentTarget)}>
+                    <MoreVertIcon/>
+                </IconButton>
+            </Box>
+            <Menu anchorEl={anchorEl} keepMounted open={anchorEl != null} onClose={() => setAnchorEl(null)}
+                  onClick={() => setAnchorEl(null)}>
                 {rightButtonAreaElements.slice().reverse().concat(leftButtonAreaElements.slice().reverse()).map(button => {
                     const {children, onClick} = button.props
                     return <MenuItem key={button.key ?? children} onClick={onClick}>{children}</MenuItem>
